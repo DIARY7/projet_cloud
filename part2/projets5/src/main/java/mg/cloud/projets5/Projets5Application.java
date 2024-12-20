2d659fb66798e074ac401d763cde9899f3993927
Ramavo Harinaivo <nyxrafaly@gmail.com>
2024-12-20 14:22:00 +0300

    spring

---
+++ part2/projets5/src/main/java/mg/cloud/projets5/Projets5Application.java
@@ -0,0 +1,13 @@
+package mg.cloud.projets5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projets5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projets5Application.class, args);
+	}
+
+}
---
+++ part2/projets5/src/test/java/mg/cloud/projets5/Projets5ApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.cloud.projets5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projets5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
