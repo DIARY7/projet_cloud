cf3e64da12d80283b2ee4c50dd54b35744680c14
Ramavo Harinaivo <nyxrafaly@gmail.com>
2025-01-29 08:52:23 +0300

    Evolution Crypto,
    Fonction à moitier terminer et pas encore tester

--- part2/projets5/src/main/java/mg/cloud/projets5/repo/PrixCryptoRepo.java
+++ part2/projets5/src/main/java/mg/cloud/projets5/repo/PrixCryptoRepo.java
@@ -1,9 +1,16 @@
 package mg.cloud.projets5.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import mg.cloud.projets5.entity.PrixCrypto;
 
 public interface PrixCryptoRepo extends JpaRepository<PrixCrypto, Integer> {
+
+
+    @Query("SELECT pc FROM PrixCrypto pc ORDER BY pc.daty ASC")
+    List<PrixCrypto> findAll();
     
 }
--- part2/projets5/src/main/java/mg/cloud/projets5/services/CryptoService.java
+++ part2/projets5/src/main/java/mg/cloud/projets5/services/CryptoService.java
@@ -1,12 +1,42 @@
 package mg.cloud.projets5.services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mg.cloud.projets5.dto.evolutionCrypto.EvolutionCryptoDTO;
+import mg.cloud.projets5.dto.evolutionCrypto.ListCryptoPrix;
+import mg.cloud.projets5.dto.evolutionCrypto.PrixDate;
+import mg.cloud.projets5.entity.Crypto;
+import mg.cloud.projets5.entity.PrixCrypto;
 import mg.cloud.projets5.repo.CryptoRepo;
+import mg.cloud.projets5.repo.PrixCryptoRepo;
 
 @Service
 public class CryptoService {
+
     @Autowired
     CryptoRepo cryptoRepo;
+
+    @Autowired
+    PrixCryptoRepo prixCryptoRepo;
+
+
+    public EvolutionCryptoDTO getEvolutionCrypto(){
+        List<PrixCrypto> prixCryptos = prixCryptoRepo.findAll();
+        EvolutionCryptoDTO evolutionCryptoDTO = new EvolutionCryptoDTO();
+        
+
+
+
+        return evolutionCryptoDTO
+
+    }
+
+
+    
 }
