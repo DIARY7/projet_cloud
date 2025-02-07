09448c1ff864d15bb6941842e08333e5885cc256
soaalice <amboararampanana@gmail.com>
2025-02-07 19:12:41 +0300

    refactorisation composant navbar

--- part3/front/src/components/NavBar.jsx
+++ part3/front/src/components/NavBar.jsx
@@ -2,36 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Bitcoin } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
-
-const Dropdown = ({ label, items, isOpen, toggleDropdown, ariaLabel }) => (
-    <div
-        className="relative"
-        onMouseEnter={() => toggleDropdown(true)} // Open on hover
-        onMouseLeave={() => toggleDropdown(false)} // Close when hover ends
-    >
-        <button
-            onBlur={() => setTimeout(() => toggleDropdown(false), 200)} // Close dropdown when focus is lost
-            className="text-white hover:text-yellow-500 focus:outline-none"
-            aria-expanded={isOpen}
-            aria-label={ariaLabel || label} // Accessibility improvement
-        >
-            {label}
-        </button>
-        {isOpen && (
-            <div className="absolute bg-white text-black p-2 mt-1 rounded-lg shadow-lg w-48 z-10">
-                {items.map((item, index) => (
-                    <Link
-                        key={index}
-                        to={item.link}
-                        className="block px-4 py-2 text-sm hover:bg-yellow-100 focus:outline-none"
-                    >
-                        {item.label}
-                    </Link>
-                ))}
-            </div>
-        )}
-    </div>
-);
+import Dropdown from './DropDown';
 
 const Navbar = () => {
     const { token, isAdmin, logout } = useAuth();
