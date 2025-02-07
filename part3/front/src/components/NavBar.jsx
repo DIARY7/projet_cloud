b1c04204ecb983e74cefd405537e91f8ed7b2870
soaalice <amboararampanana@gmail.com>
2025-02-07 19:11:08 +0300

    Update NavBar.jsx

--- part3/front/src/components/NavBar.jsx
+++ part3/front/src/components/NavBar.jsx
@@ -1,22 +1,60 @@
-import React, { useState , useEffect} from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { Bitcoin } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
 
+const Dropdown = ({ label, items, isOpen, toggleDropdown, ariaLabel }) => (
+    <div
+        className="relative"
+        onMouseEnter={() => toggleDropdown(true)} // Open on hover
+        onMouseLeave={() => toggleDropdown(false)} // Close when hover ends
+    >
+        <button
+            onBlur={() => setTimeout(() => toggleDropdown(false), 200)} // Close dropdown when focus is lost
+            className="text-white hover:text-yellow-500 focus:outline-none"
+            aria-expanded={isOpen}
+            aria-label={ariaLabel || label} // Accessibility improvement
+        >
+            {label}
+        </button>
+        {isOpen && (
+            <div className="absolute bg-white text-black p-2 mt-1 rounded-lg shadow-lg w-48 z-10">
+                {items.map((item, index) => (
+                    <Link
+                        key={index}
+                        to={item.link}
+                        className="block px-4 py-2 text-sm hover:bg-yellow-100 focus:outline-none"
+                    >
+                        {item.label}
+                    </Link>
+                ))}
+            </div>
+        )}
+    </div>
+);
+
 const Navbar = () => {
     const { token, isAdmin, logout } = useAuth();
-
     const isAuth = token != null;
 
-    const [isTransactionsOpen, setIsTransactionsOpen] = useState(false);
-    const [isCommissionsOpen, setIsCommissionsOpen] = useState(false);
-    const [isCryptosOpen, setIsCryptosOpen] = useState(false);
-    const [isWalletOpen, setIsWalletOpen] = useState(false);
+    const [dropdowns, setDropdowns] = useState({
+        transactions: false,
+        commissions: false,
+        cryptos: false,
+        wallet: false,
+    });
+
+    const toggleDropdown = (dropdown, state) => {
+        setDropdowns((prevState) => ({
+            ...prevState,
+            [dropdown]: state,
+        }));
+    };
 
     return (
         <nav className="container mx-auto px-6 py-4">
             <div className="flex items-center justify-between">
-                <Link to='/'>
+                <Link to="/">
                     <div className="flex items-center">
                         <Bitcoin className="h-8 w-8 text-yellow-500" />
                         <span className="ml-2 text-xl font-bold text-white">CryptoTrade</span>
@@ -24,101 +62,78 @@ const Navbar = () => {
                 </Link>
                 <div className="flex items-center space-x-4">
                     {!isAuth ? (
-                        // Si non authentifié
                         <>
-                            <Link to="/login" className="text-white hover:text-yellow-500">Connexion</Link>
-                            <Link to="/register" className="bg-yellow-500 px-4 py-2 rounded-lg text-gray-900 hover:bg-yellow-400">
+                            <Link to="/login" className="text-white hover:text-yellow-500">
+                                Connexion
+                            </Link>
+                            <Link
+                                to="/register"
+                                className="bg-yellow-500 px-4 py-2 rounded-lg text-gray-900 hover:bg-yellow-400"
+                            >
                                 Inscription
                             </Link>
                         </>
                     ) : isAdmin ? (
-                        // Si authentifié et admin
                         <>
-                            <div className="relative">
-                                <button
-                                    onClick={() => setIsTransactionsOpen(!isTransactionsOpen)}
-                                    className="text-white hover:text-yellow-500"
-                                >
-                                    Transactions
-                                </button>
-                                {isTransactionsOpen && (
-                                    <div className="absolute bg-white text-black p-2 rounded-lg shadow-lg">
-                                        <Link to="/transactions/resume" className="block">Résumé</Link>
-                                        <Link to="/transactions" className="block">Liste</Link>
-                                        <Link to="/transactions/validation" className="block">Validation</Link>
-                                    </div>
-                                )}
-                            </div>
-                            <div className="relative">
-                                <button
-                                    onClick={() => setIsCommissionsOpen(!isCommissionsOpen)}
-                                    className="text-white hover:text-yellow-500"
-                                >
-                                    Commissions
-                                </button>
-                                {isCommissionsOpen && (
-                                    <div className="absolute bg-white text-black p-2 rounded-lg shadow-lg">
-                                        <Link to="/commissions/analysis" className="block">Analyse</Link>
-                                        <Link to="/commissions/edit" className="block">Configuration</Link>
-                                    </div>
-                                )}
-                            </div>
-                            <div className="relative">
-                                <button
-                                    onClick={() => setIsCryptosOpen(!isCryptosOpen)}
-                                    className="text-white hover:text-yellow-500"
-                                >
-                                    Cryptos
-                                </button>
-                                {isCryptosOpen && (
-                                    <div className="absolute bg-white text-black p-2 rounded-lg shadow-lg">
-                                        <Link to="/cryptos/analysis" className="block">Analyse</Link>
-                                        <Link to="/cryptos/prices" className="block">Cours</Link>
-                                        <Link to="/cryptos/evolution" className="block">Évolution</Link>
-                                    </div>
-                                )}
-                            </div>
+                            <Dropdown
+                                label="Transactions"
+                                isOpen={dropdowns.transactions}
+                                toggleDropdown={(state) => toggleDropdown('transactions', state)}
+                                items={[
+                                    { label: 'Résumé', link: '/transactions/resume' },
+                                    { label: 'Liste', link: '/transactions' },
+                                    { label: 'Validation', link: '/transactions/validation' },
+                                ]}
+                            />
+                            <Dropdown
+                                label="Commissions"
+                                isOpen={dropdowns.commissions}
+                                toggleDropdown={(state) => toggleDropdown('commissions', state)}
+                                items={[
+                                    { label: 'Analyse', link: '/commissions/analysis' },
+                                    { label: 'Configuration', link: '/commissions/edit' },
+                                ]}
+                            />
+                            <Dropdown
+                                label="Cryptos"
+                                isOpen={dropdowns.cryptos}
+                                toggleDropdown={(state) => toggleDropdown('cryptos', state)}
+                                items={[
+                                    { label: 'Analyse', link: '/cryptos/analysis' },
+                                    { label: 'Cours', link: '/cryptos/prices' },
+                                    { label: 'Évolution', link: '/cryptos/evolution' },
+                                ]}
+                            />
                             <button
-                                onClick={logout} // Appeler la fonction de déconnexion
+                                onClick={logout}
                                 className="text-white hover:text-yellow-500"
                             >
                                 Déconnexion
                             </button>
                         </>
                     ) : (
-                        // Si authentifié mais non admin
                         <>
-                            <div className="relative">
-                                <button
-                                    onClick={() => setIsWalletOpen(!isWalletOpen)}
-                                    className="text-white hover:text-yellow-500"
-                                >
-                                    Wallet
-                                </button>
-                                {isWalletOpen && (
-                                    <div className="absolute bg-white text-black p-2 rounded-lg shadow-lg">
-                                        <Link to="/wallet/funds" className="block">Fonds</Link>
-                                        <Link to="/wallet/cryptos" className="block">Cryptos</Link>
-                                    </div>
-                                )}
-                            </div>
-                            <div className="relative">
-                                <button
-                                    onClick={() => setIsCryptosOpen(!isCryptosOpen)}
-                                    className="text-white hover:text-yellow-500"
-                                >
-                                    Cryptos
-                                </button>
-                                {isCryptosOpen && (
-                                    <div className="absolute bg-white text-black p-2 rounded-lg shadow-lg">
-                                        <Link to="/cryptos/analysis" className="block">Analyse</Link>
-                                        <Link to="/cryptos/prices" className="block">Prix</Link>
-                                        <Link to="/cryptos/evolution" className="block">Évolution</Link>
-                                    </div>
-                                )}
-                            </div>
+                            <Dropdown
+                                label="Wallet"
+                                isOpen={dropdowns.wallet}
+                                toggleDropdown={(state) => toggleDropdown('wallet', state)}
+                                items={[
+                                    { label: 'Fonds', link: '/wallet/funds' },
+                                    { label: 'Cryptos', link: '/wallet/cryptos' },
+                                ]}
+                            />
+                            <Dropdown
+                                label="Cryptos"
+                                isOpen={dropdowns.cryptos}
+                                toggleDropdown={(state) => toggleDropdown('cryptos', state)}
+                                items={[
+                                    { label: 'Analyse', link: '/cryptos/analysis' },
+                                    { label: 'Prix', link: '/cryptos/prices' },
+                                    { label: 'Évolution', link: '/cryptos/evolution' },
+                                ]}
+                            />
                             <button
-                                onClick={logout} // Appeler la fonction de déconnexion
+                                onClick={logout}
                                 className="text-white hover:text-yellow-500"
                             >
                                 Déconnexion
@@ -129,6 +144,6 @@ const Navbar = () => {
             </div>
         </nav>
     );
-}
+};
 
 export default Navbar;
