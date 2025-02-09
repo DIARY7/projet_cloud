7746281edb2bfce10759804a1c7f3603268ac916
Ramavo Harinaivo <nyxrafaly@gmail.com>
2025-02-09 09:51:41 +0300

    sychro crypto et prix crypto
    mbola mandefa fa mbola tsy par rapport à une date

--- part2/projets5/src/main/java/mg/cloud/projets5/services/SynchronisationService.java
+++ part2/projets5/src/main/java/mg/cloud/projets5/services/SynchronisationService.java
@@ -6,17 +6,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteBatch;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.UserRecord;
 import com.google.firebase.auth.UserRecord.CreateRequest;
 import com.google.firebase.cloud.FirestoreClient;
 
+import mg.cloud.projets5.dto.coursCrypto.CoursCryptoDTO;
+import mg.cloud.projets5.entity.Crypto;
+import mg.cloud.projets5.entity.PrixCrypto;
+import mg.cloud.projets5.entity.TransactionCrypto;
 import mg.cloud.projets5.entity.Users;
 
 @Service
@@ -24,6 +35,12 @@ public class SynchronisationService {
 
     @Autowired
     UsersService usersService;
+
+    @Autowired
+    CryptoService cryptoService;
+
+    @Autowired
+    TransactionCryptoService transactionCryptoService;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -42,6 +59,144 @@ public class SynchronisationService {
     }
 
     private void synchroToOnline(LocalDateTime lastSync,Firestore db) throws Exception {
+        
+        // localToFirestoreUsers(lastSync, db);
+
+        // crypto
+        localToFirestoreCrypto(db);
+        // prix_crypto
+        localToFirestorePrixCrypto(lastSync,db);
+
+        // transactio_crypto 
+        // localToFirestoretransactionCrypto(lastSync,db);
+    }
+
+    private void updateDate() {
+        LocalDateTime now = LocalDateTime.now();
+        String sql = "INSERT INTO synchro_date (dt_sync) VALUES (?)";
+        jdbcTemplate.update(sql, now);
+    }
+    
+
+    private LocalDateTime getLastSync() {
+        String sql = "SELECT MAX(dt_sync) FROM synchro_date";
+        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
+            Timestamp timestamp = rs.getTimestamp(1);
+            return timestamp != null ? timestamp.toLocalDateTime() : null;
+        });
+    }
+
+
+    private void localToFirestoreCrypto(Firestore db) throws Exception {
+        List<Crypto> cryptos = cryptoService.findAll();
+    
+        // Effacer l'ancienne collection avant l'insertion
+        clearCollection("crypto", db);
+    
+        WriteBatch batch = db.batch();
+        int count = 0;
+    
+        for (Crypto crypto : cryptos) {
+            Map<String, Object> cryptoMap = new HashMap<>();
+            cryptoMap.put("crypto_id", crypto.getId());
+            cryptoMap.put("label", crypto.getLabel());
+    
+            // Préparer la requête Firestore
+            DocumentReference docRef = db.collection("crypto").document(UUID.randomUUID().toString());
+            batch.set(docRef, cryptoMap);
+    
+            count++;
+    
+            // Commit du batch tous les 100 documents
+            if (count == 100) {
+                batch.commit().get();  // Exécution du batch
+                batch = db.batch();    // Nouveau batch
+                count = 0;
+            }
+        }
+    
+        // Commit des restants si nécessaire
+        if (count > 0) {
+            batch.commit().get();
+        }
+    
+        System.out.println("Les données Crypto ont été synchronisées.");
+    }
+    
+    private void localToFirestoretransactionCrypto(LocalDateTime lastSync, Firestore db) throws Exception {
+        // à changer aveo
+        List<TransactionCrypto> listTransactionCryptos = transactionCryptoService.filterByUserIdAndDateAndCryptoId(null,null,null,null);
+        
+        WriteBatch batch = db.batch();
+        int count = 0;
+
+        for (TransactionCrypto transactionCrypto : listTransactionCryptos) {
+
+            Map<String,Object> transactionCryptoMap = new HashMap<>();
+            transactionCryptoMap.put("crypto_id", transactionCrypto.getCrypto().getId());
+            transactionCryptoMap.put("crypto_nom", transactionCrypto.getCrypto().getLabel());
+            transactionCryptoMap.put("dt_transaction", Timestamp.valueOf(transactionCrypto.getDtTransaction()));
+            transactionCryptoMap.put("is_achat", false);
+            if(transactionCrypto.getCommission().getId() == 2) transactionCryptoMap.put("is_achat", true);
+            transactionCryptoMap.put("pu_crypto", transactionCrypto.getPuCrypto());
+            transactionCryptoMap.put("qte", transactionCrypto.getQte());
+            transactionCryptoMap.put("user_id", transactionCrypto.getUsers().getId());
+
+            // Préparer l'insertion dans le batch
+            DocumentReference docRef = db.collection("transaction_crypto").document(UUID.randomUUID().toString());
+            batch.set(docRef, transactionCryptoMap);
+            count++;
+
+             // Limite de Firestore : 500 opérations par batch
+            if (count == 100) {
+                batch.commit().get();
+                batch = db.batch();  // Créer un nouveau batch
+                count = 0;
+            }
+        }
+        if (count > 0) {
+            batch.commit().get();
+        }
+        System.out.println("Les données transaction crypto ont été synchronisées.");
+    }
+
+
+    private void localToFirestorePrixCrypto(LocalDateTime lastSync, Firestore db) throws Exception {
+    clearCollection("prix_crypto", db);
+    CoursCryptoDTO coursCryptoDTO = cryptoService.getCoursCrypto();
+
+    WriteBatch batch = db.batch();
+    int count = 0;
+
+    for (PrixCrypto prixCrypto : coursCryptoDTO.getCryptoPrix()) {
+        Map<String, Object> prixCryptoData = new HashMap<>();
+        prixCryptoData.put("crypto_id", prixCrypto.getCrypto().getId());
+        prixCryptoData.put("crypto_nom", prixCrypto.getCrypto().getLabel());
+        prixCryptoData.put("daty", Timestamp.valueOf(prixCrypto.getDaty()));
+        prixCryptoData.put("prix", prixCrypto.getPrix());
+
+        // Préparer l'insertion dans le batch
+        DocumentReference docRef = db.collection("prix_crypto").document(UUID.randomUUID().toString());
+        batch.set(docRef, prixCryptoData);
+        count++;
+
+        // Limite de Firestore : 500 opérations par batch
+        if (count == 100) {
+            batch.commit().get();
+            batch = db.batch();  // Créer un nouveau batch
+            count = 0;
+        }
+    }
+
+    // Commit des insertions restantes
+    if (count > 0) {
+        batch.commit().get();
+    }
+    System.out.println("Les données prix_crypto ont été synchronisées.");
+}
+
+
+    private void localToFirestoreUsers(LocalDateTime lastSync,Firestore db)throws Exception{
         // users
         List<Users> newUsers = usersService.getUsersAddedAfter(lastSync);
         for (Users users : newUsers) {
@@ -67,25 +222,48 @@ public class SynchronisationService {
 
         }
 
-        // crypto
 
-        // prix_crypto
-
-        // transactio_crypto 
+        
     }
 
-    private void updateDate() {
-        LocalDateTime now = LocalDateTime.now();
-        String sql = "INSERT INTO synchro_date (dt_sync) VALUES (?)";
-        jdbcTemplate.update(sql, now);
-    }
-    
+    public void clearCollection(String collectionName, Firestore db) {
+    try {
+        CollectionReference collection = db.collection(collectionName);
 
-    private LocalDateTime getLastSync() {
-        String sql = "SELECT MAX(dt_sync) FROM synchro_date";
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
-            Timestamp timestamp = rs.getTimestamp(1);
-            return timestamp != null ? timestamp.toLocalDateTime() : null;
-        });
+        // Obtenir tous les documents
+        ApiFuture<QuerySnapshot> future = collection.get();
+        QuerySnapshot documents = future.get();
+
+        if (documents.isEmpty()) {
+            System.out.println("La collection est déjà vide.");
+            return;
+        }
+
+        WriteBatch batch = db.batch();
+        int count = 0;
+
+        // Ajouter les suppressions au batch
+        for (DocumentSnapshot document : documents) {
+            batch.delete(document.getReference());
+            count++;
+
+            // Limite de Firestore : 500 opérations par batch
+            if (count == 100) {
+                batch.commit().get();  // Exécute le batch actuel
+                batch = db.batch();     // Réinitialise le batch
+                count = 0;
+            }
+        }
+
+        // Commit des suppressions restantes
+        if (count > 0) {
+            batch.commit().get();
+        }
+
+        System.out.println("La collection " + collectionName + " a été vidée.");
+    } catch (InterruptedException | ExecutionException e) {
+        e.printStackTrace();
     }
+}
+
 }
